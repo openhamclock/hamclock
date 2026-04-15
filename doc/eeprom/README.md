3d63cc12bc707cd48ab263db48bc73b4fc409480
Paul Gallagher <gallagher.paul@gmail.com>
2026-04-15 15:08:38 +0800

    update eeprom doc generator so it can be used in-situ

--- doc/eeprom/README.md
+++ doc/eeprom/README.md
@@ -2,7 +2,7 @@
 
 The HamClock Client uses an "EEPROM" file  to store configuration settings. It is located in `~/.hamclock/eeprom` of a client installation.
 
-See [hceeprom.csv](./hceeprom.csv) for the definition of all the data elements in the file. It is generated with [nvtocsv.bash](./nvtocsv.bash).
+See [hceeprom.csv](./hceeprom.csv) for the definition of all the data elements in the file. It is created/updated with [generate.sh](./generate.sh).
 
 The objective for documenting this file is so users can switch to other HamClock implementations and maintain their configuration.
 
--- doc/eeprom/generate.sh
+++ doc/eeprom/generate.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-perl hceeprom.pl -a | grep NV_ >hceeprom.log
-echo Addr,Name,Len,Type,Description >hceeprom.csv
+script_path=${0%/*}/
+src_path=${script_path}../../ESPHamClock/
+log_file=../doc/eeprom/hceeprom.log
+csv_file=../doc/eeprom/hceeprom.csv
+contrib_path=../hamclock-contrib/
+
+cd ${src_path}
+perl ${contrib_path}hceeprom.pl -a | grep NV_ > ${log_file}
+echo Addr,Name,Len,Type,Description > ${csv_file}
 awk '
 BEGIN { FS=" " ; OFS="," }
 {
@@ -10,21 +17,20 @@ BEGIN { FS=" " ; OFS="," }
     f2 = $2
     f3 = $3
     f4 = $4
-    
+
     # Rebuild field 5 from field 5 to end
     f5 = ""
     for (i=5; i<=NF; i++) {
         if (f5 != "") f5 = f5 " "
         f5 = f5 $i
     }
-    
+
     # Properly quote field 5 if needed
     if (index(f5, ",") || index(f5, "\"") || index(f5, "\n")) {
         gsub(/"/, "\"\"", f5)
         f5 = "\"" f5 "\""
     }
-    
+
     print f1, f2, f3, f4, f5
-}' hceeprom.log >>hceeprom.csv
-
-
+}' ${log_file} >> ${csv_file}
+rm ${log_file}
